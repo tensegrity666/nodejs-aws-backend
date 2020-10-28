56b88787e01956f4e362de5f3e944a170d0b5fde
Roman Yakovlev <tensegrity666@icloud.com>
2020-10-28 22:52:52 +0300

    feat: setup serverless

---
+++ product-service/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+// eslint-disable-next-line import/no-unresolved
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  output: {
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.join(__dirname, '.webpack'),
+  },
+  mode: 'development',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: __dirname,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      node: 'current',
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
