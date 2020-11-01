7a620e8f81637b1fa93bb1a51ca40ae48ccade6c
Roman Yakovlev <tensegrity666@icloud.com>
2020-11-01 20:07:16 +0300

    feat: add try-catch

--- open-weather-service/handler.js
+++ open-weather-service/handler.js
@@ -1,24 +1,31 @@
 import fetch from 'node-fetch';
 
 export const getWeather = async (event, context) => {
-  const response = await fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=17622236e3b5352e5d47b38be02d682f"
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=17622236e3b5352e5d47b38be02d682f"
+    );
+    const data = await response.json();
 
-  const { name, weather, sys, main } = data;
-  const { country } = sys;
-  const { description } = weather[0];
-  const { temp } = main;
+    const { name, weather, sys, main } = data;
+    const { country } = sys;
+    const { description } = weather[0];
+    const { temp } = main;
 
-  const forecast =
-    `${name}, ${country} ` +
-    `weather: ${description} ` +
-    `temperature: ${Math.round(Number(temp) - 273.15)} C ` +
-    `via https://openweathermap.org/current`;
+    const forecast =
+      `${name}, ${country} ` +
+      `weather: ${description} ` +
+      `temperature: ${Math.round(Number(temp) - 273.15)}C ` +
+      `via https://openweathermap.org/current`;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(forecast, null, 2),
-  };
+    return {
+      statusCode: 200,
+      body: forecast,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Error',
+    };
+  }
 };
--- product-service/handlers/products.js
+++ product-service/handlers/products.js
@@ -1,14 +1,21 @@
 import productsMock from "../mocks/productsMock";
 
 const getProductsList = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-    body: JSON.stringify(productsMock, null, 2),
-  };
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(productsMock, null, 2),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Error",
+    };
+  }
 };
 
 export { getProductsList };
