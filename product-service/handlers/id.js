969631a08d7f46a9da616edb5890c3acad6c797c
Roman Yakovlev <tensegrity666@icloud.com>
2020-11-01 20:07:16 +0300

    feat: add try-catch

--- product-service/handlers/id.js
+++ product-service/handlers/id.js
@@ -1,18 +1,25 @@
 import products from "../mocks/productsMock";
 
 const getProductById = async (event, context) => {
-  const product = products.filter(
-    (element) => element.id === event.pathParameters.id
-  );
+  try {
+    const product = products.filter(
+      (element) => element.id === event.pathParameters.id
+    );
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-    body: JSON.stringify(product, null, 2),
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(product, null, 2),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Error",
+    };
+  }
 };
 
 export { getProductById };
